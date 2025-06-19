216b72fcc27f834908a62fed535d5402e510034b
praveensource <praveenks2604@gmail.com>
2025-06-19 18:23:00 +0530

    create header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,56 @@
+import {Button, Navbar, NavbarLink, TextInput, NavbarCollapse, NavbarToggle} from 'flowbite-react';
+import { Link, useLocation } from 'react-router-dom';
+import {AiOutlineSearch} from 'react-icons/ai'
+import {FaMoon} from 'react-icons/fa'
+
+const Header = () => {
+
+    const path = useLocation().pathname;
+
+  return (
+    <Navbar className='dark:bg-white border-b-2'>
+        <Link to={'/'} className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Praveen's</span>
+            Blog
+        </Link>
+
+        {/* search */}
+        <form>
+            <TextInput  type='text' placeholder='Search...' rightIcon={AiOutlineSearch} className='hidden lg:inline'  />
+        </form>
+
+        <Button className='w-12 h-10 lg:hidden' pill color='gray'>
+            <AiOutlineSearch />
+        </Button>
+
+        {/* for signin and dark mode */}
+        <div className='flex gap-2 md:order-2'>
+            <Button className='w-12 h-10 hidden sm:inline' color={'gray'} pill>
+                <FaMoon />
+            </Button>
+            <Link to={'/sign-in'}>
+                <Button className='bg-gradient-to-br from-purple-600 to-blue-500 text-white hover:bg-gradient-to-bl focus:ring-blue-300 dark:focus:ring-blue-800'>
+                    Sign In
+                </Button>
+            </Link>
+            <NavbarToggle />
+
+        </div>
+        {/* Menu */}
+            <NavbarCollapse>
+                <NavbarLink active={path == '/'} as={'div'}>
+                    <Link to={'/'}>Home</Link>
+                </NavbarLink>
+                <NavbarLink active={path=='/about'} as={'div'}>
+                    <Link to={'/about'}>About</Link>
+                </NavbarLink>
+                <NavbarLink active= {path=='/projects'} as={'div'}>
+                    <Link to={'/projects'}>Projects</Link>
+                </NavbarLink>
+                
+            </NavbarCollapse>
+    </Navbar>
+  )
+}
+
+export default Header
