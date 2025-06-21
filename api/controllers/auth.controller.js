8f437e69d2648f33c8dcb1e2d8996c7fc22bcc81
praveensource <praveenks2604@gmail.com>
2025-06-22 01:15:42 +0530

    add middlware to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req,res) => {
+export const signup = async (req,res,next) => {
     const {username, email, password} = req.body;
 
     if (!username || !password || !email || username === '' || password === '' || email ===''){
-        return res.status(400).json({message:"All fields are required"});
+        next(errorHandler(400,'all fields are required'));
 
     }
 
@@ -21,7 +22,7 @@ export const signup = async (req,res) => {
         res.json({message:"Signup successful"});
 
     } catch (error) {
-        res.status(500).json({error});
+        next(error);
     }
 
 
--- api/index.js
+++ api/index.js
@@ -23,3 +23,13 @@ app.use(express.json())
 
 app.use('/api/user',userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err,req,res,next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'internal Server Error';
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
