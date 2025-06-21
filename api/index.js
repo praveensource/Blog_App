37f3358198864aa263441e5c40c3309c98028e37
praveensource <praveenks2604@gmail.com>
2025-06-22 01:00:30 +0530

    create signup api routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(
@@ -18,4 +19,7 @@ app.listen(3000, () => {
     console.log("Running in host 3000!! :)");
 })
 
+app.use(express.json())
+
 app.use('/api/user',userRoutes);
+app.use('/api/auth', authRoutes);
