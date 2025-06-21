3a4a4387d633db1b21bf41a7c8438f4d82be83a8
praveensource <praveenks2604@gmail.com>
2025-06-21 23:44:52 +0530

    create a test api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,9 +16,7 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
     },
-
 }, {timestamps:true})
-
 
 // create a model
 const User = mongoose.model('User', userSchema);
